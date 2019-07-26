8cb243b9cd78f7d89a335a65bd66ed70bd0ad659
9527 <361426201@qq.com>
2019-07-26 10:19:54 +0800

    初始化

---
+++ src/main/java/com/ymy/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.ymy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(){
+
+        return "HELLO-WORLD";
+    }
+}
